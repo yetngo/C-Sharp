efa37127b1426e49f28ad6ace64ccd697c35e3f1
yetngo <pebitrontron@gmail.com>
2020-11-14 16:17:42 +0700

    Lab 3 done

---
+++ Lab3/Lab3/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lsvEmployee = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.cbbYear = new System.Windows.Forms.ComboBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.dtpJoin = new System.Windows.Forms.DateTimePicker();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.llb = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // lsvEmployee
+            // 
+            this.lsvEmployee.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.lsvEmployee.FullRowSelect = true;
+            this.lsvEmployee.GridLines = true;
+            this.lsvEmployee.HideSelection = false;
+            this.lsvEmployee.Location = new System.Drawing.Point(13, 12);
+            this.lsvEmployee.Name = "lsvEmployee";
+            this.lsvEmployee.Size = new System.Drawing.Size(530, 324);
+            this.lsvEmployee.TabIndex = 0;
+            this.lsvEmployee.UseCompatibleStateImageBehavior = false;
+            this.lsvEmployee.View = System.Windows.Forms.View.Details;
+            this.lsvEmployee.SelectedIndexChanged += new System.EventHandler(this.lsvEmployee_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Age";
+            this.columnHeader2.Width = 40;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Address";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Expertience(Year)";
+            this.columnHeader4.Width = 40;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Phone";
+            this.columnHeader5.Width = 80;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Email";
+            this.columnHeader6.Width = 90;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Date of Joining";
+            this.columnHeader7.Width = 80;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(560, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(560, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(560, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(560, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Years of Experience";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(560, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Phone number";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(560, 196);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Email ID";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(560, 227);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Date of Joining";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(668, 30);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(191, 20);
+            this.txtName.TabIndex = 8;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(668, 63);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(191, 20);
+            this.txtAge.TabIndex = 9;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(668, 98);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(191, 20);
+            this.txtAddress.TabIndex = 10;
+            // 
+            // cbbYear
+            // 
+            this.cbbYear.FormattingEnabled = true;
+            this.cbbYear.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2"});
+            this.cbbYear.Location = new System.Drawing.Point(668, 131);
+            this.cbbYear.Name = "cbbYear";
+            this.cbbYear.Size = new System.Drawing.Size(191, 21);
+            this.cbbYear.TabIndex = 11;
+            this.cbbYear.Text = "0";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(668, 163);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(191, 20);
+            this.txtPhone.TabIndex = 12;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(668, 193);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(191, 20);
+            this.txtEmail.TabIndex = 13;
+            // 
+            // dtpJoin
+            // 
+            this.dtpJoin.CustomFormat = "MM-dd-yyyy";
+            this.dtpJoin.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpJoin.Location = new System.Drawing.Point(668, 221);
+            this.dtpJoin.Name = "dtpJoin";
+            this.dtpJoin.Size = new System.Drawing.Size(191, 20);
+            this.dtpJoin.TabIndex = 14;
+            this.dtpJoin.Value = new System.DateTime(2020, 11, 14, 16, 2, 19, 0);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(549, 273);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 15;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(630, 273);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 16;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(711, 273);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 17;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(792, 273);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 18;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // llb
+            // 
+            this.llb.AutoSize = true;
+            this.llb.Location = new System.Drawing.Point(686, 323);
+            this.llb.Name = "llb";
+            this.llb.Size = new System.Drawing.Size(46, 13);
+            this.llb.TabIndex = 19;
+            this.llb.TabStop = true;
+            this.llb.Text = "Website";
+            this.llb.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llb_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(871, 364);
+            this.Controls.Add(this.llb);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dtpJoin);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.cbbYear);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lsvEmployee);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lsvEmployee;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.ComboBox cbbYear;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.DateTimePicker dtpJoin;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.LinkLabel llb;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+    }
+}
+
---
+++ Lab3/Lab3/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text) || string.IsNullOrWhiteSpace(txtAge.Text) || string.IsNullOrWhiteSpace(txtAddress.Text)
+                || string.IsNullOrWhiteSpace(txtEmail.Text) || string.IsNullOrWhiteSpace(txtPhone.Text)
+                || string.IsNullOrWhiteSpace(cbbYear.SelectedIndex.ToString()) || string.IsNullOrWhiteSpace(dtpJoin.Value.ToString()))
+                MessageBox.Show("Please fill all text boxes!");
+            else
+            {
+                ListViewItem lvi = lsvEmployee.Items.Add(txtName.Text);
+                lvi.SubItems.Add(txtAge.Text);
+                lvi.SubItems.Add(txtAddress.Text);
+                lvi.SubItems.Add(cbbYear.Text);
+                lvi.SubItems.Add(txtPhone.Text);
+                lvi.SubItems.Add(txtEmail.Text);
+                lvi.SubItems.Add(dtpJoin.Value.ToShortDateString());
+                MessageBox.Show("Add success");
+            }
+        }
+        public void SaveFile(string filename)
+        {
+            StreamWriter write = new StreamWriter(filename);
+            foreach (ListViewItem item in lsvEmployee.Items)
+                write.WriteLineAsync(item.SubItems[0].Text+","+item.SubItems[1].Text + "," + item.SubItems[2].Text
+                    + "," + item.SubItems[3].Text + "," + item.SubItems[4].Text + "," + item.SubItems[5].Text + "," + item.SubItems[6].Text);
+            write.ToString();
+            write.Close();
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.DefaultExt = "txt";
+            saveDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveDialog.FilterIndex = 2;
+            saveDialog.RestoreDirectory = true;
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                SaveFile(saveDialog.FileName);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lsvEmployee.SelectedItems.Count > 0)
+            {
+                lsvEmployee.Items.Remove(lsvEmployee.SelectedItems[0]);
+                MessageBox.Show("Delete success");
+            }
+            else MessageBox.Show("Please select a employee!");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (lsvEmployee.SelectedItems.Count > 0)
+            {
+                lsvEmployee.SelectedItems[0].SubItems[0].Text = txtName.Text;
+                lsvEmployee.SelectedItems[0].SubItems[1].Text = txtAge.Text;
+                lsvEmployee.SelectedItems[0].SubItems[2].Text = txtAddress.Text;
+                lsvEmployee.SelectedItems[0].SubItems[3].Text = cbbYear.Text;
+                lsvEmployee.SelectedItems[0].SubItems[4].Text = txtPhone.Text;
+                lsvEmployee.SelectedItems[0].SubItems[5].Text = txtEmail.Text;
+                lsvEmployee.SelectedItems[0].SubItems[6].Text = dtpJoin.Text;
+                MessageBox.Show("Update success");
+            }
+            else MessageBox.Show("Please select a employee!");
+        }
+
+        private void llb_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.llb.LinkVisited = true;
+            System.Diagnostics.Process.Start("http://talktime.com");
+        }
+
+        private void lsvEmployee_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lsvEmployee.SelectedItems.Count > 0)
+            {
+                txtName.Text = lsvEmployee.SelectedItems[0].SubItems[0].Text;
+                txtAge.Text = lsvEmployee.SelectedItems[0].SubItems[1].Text;
+                txtAddress.Text = lsvEmployee.SelectedItems[0].SubItems[2].Text;
+                cbbYear.Text = lsvEmployee.SelectedItems[0].SubItems[3].Text;
+                txtPhone.Text = lsvEmployee.SelectedItems[0].SubItems[4].Text;
+                txtEmail.Text = lsvEmployee.SelectedItems[0].SubItems[5].Text;
+                dtpJoin.Text = lsvEmployee.SelectedItems[0].SubItems[6].Text;
+            }
+        }
+    }
+}
